d8db8a140eeadc697a509c1188d4700a6ff504c6
faithkigen <kigenfaith20@gmail.com>
2023-03-16 13:56:49 +0300

    completed assignment

--- spec/requests/cheeses_spec.rb
+++ spec/requests/cheeses_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Cheeses", type: :request do
   describe 'GET /cheeses/:id' do
     let!(:cheese) { Cheese.create!(name: "Cheddar", price: 3, is_best_seller: true) }
-
+    
     it 'returns the cheese with the matching id' do
       get "/cheeses/#{cheese.id}"
 
